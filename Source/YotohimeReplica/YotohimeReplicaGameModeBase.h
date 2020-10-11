be6e6dbb58d5eb222b5084b0a79175afa35662fb
Siting Pan <psting3456@gmail.com>
2020-10-11 17:51:59 -0400

    Initialize Repository

---
+++ Source/YotohimeReplica/YotohimeReplicaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YotohimeReplicaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YOTOHIMEREPLICA_API AYotohimeReplicaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
